ce1bb7d9ccbfa372b12f0400f896c62c48800389
STBoyden <sam@stboyden.com>
2021-03-11 00:20:31 +0000

    Reformatted types.h, made T_hash funcs take const pointers to T

--- types.h
+++ types.h
@@ -10,7 +10,9 @@ typedef char *str;
 // integers
 #ifndef __cplusplus
 typedef unsigned char bool;
-enum bool { false = 0, true = 1 };
+enum bool {
+    false = 0, true = 1
+};
 #else
 #error These headers are intended for C projects only.
 #endif
@@ -35,40 +37,66 @@ typedef double f64;
 // the better way is? absolutely not. the joys of generic programming in C i
 // guess.
 static void str_free(str *s) {}
+
 static void bool_free(bool *b) {}
+
 static void u8_free(u8 *u) {}
+
 static void u16_free(u16 *u) {}
+
 static void u32_free(u32 *u) {}
+
 static void u64_free(u64 *u) {}
+
 static void u128_free(u128 *u) {}
+
 static void i8_free(i8 *i) {}
+
 static void i16_free(i16 *i) {}
+
 static void i32_free(i32 *i) {}
+
 static void i64_free(i64 *i) {}
+
 static void i128_free(i128 *i) {}
+
 static void f32_free(f32 *f) {}
+
 static void f64_free(f64 *f) {}
 
 static size_t str_hash(str s) {
 
     u64 hash = 5381;
-    int c;
+    u8 c;
 
     while ((c = *s++))
         hash = ((hash << 5) + hash) + c;
 
     return hash;
 }
-static bool bool_hash(bool *b) { return *b; }
-static u8 u8_hash(u8 *u) { return *u; }
-static u16 u16_hash(u16 *u) { return *u; }
-static u32 u32_hash(u32 *u) { return *u; }
-static u64 u64_hash(u64 *u) { return *u; }
-static u128 u128_hash(u128 *u) { return *u; }
-static i8 i8_hash(i8 *i) { return *i; }
-static i16 i16_hash(i16 *i) { return *i; }
-static i32 i32_hash(i32 *i) { return *i; }
-static i64 i64_hash(i64 *i) { return *i; }
-static i128 i128_hash(i128 *i) { return *i; }
-static f32 f32_hash(f32 *f) { return *f; }
-static f64 f64_hash(f64 *f) { return *f; }
+
+static bool bool_hash(const bool *b) { return *b; }
+
+static u8 u8_hash(const u8 *u) { return *u; }
+
+static u16 u16_hash(const u16 *u) { return *u; }
+
+static u32 u32_hash(const u32 *u) { return *u; }
+
+static u64 u64_hash(const u64 *u) { return *u; }
+
+static u128 u128_hash(const u128 *u) { return *u; }
+
+static i8 i8_hash(const i8 *i) { return *i; }
+
+static i16 i16_hash(const i16 *i) { return *i; }
+
+static i32 i32_hash(const i32 *i) { return *i; }
+
+static i64 i64_hash(const i64 *i) { return *i; }
+
+static i128 i128_hash(const i128 *i) { return *i; }
+
+static f32 f32_hash(const f32 *f) { return *f; }
+
+static f64 f64_hash(const f64 *f) { return *f; }
