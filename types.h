b3b986482b9b38b93c3a3a9cec2153b77e5f22ee
STBoyden <sam@stboyden.com>
2021-04-08 20:45:44 +0100

    Define size_t if not present

--- types.h
+++ types.h
@@ -6,32 +6,40 @@
 // aliases for primitives
 
 // str - stack allocated string
-typedef char *str;
+typedef char* str;
+
+#define STRING_SIZE_MAX 100
 
 // integers
 #ifndef __cplusplus
-typedef unsigned char bool;
-enum bool { false = 0, true = 1 };
+    typedef unsigned char bool;
+    enum bool { false = 0, true = 1 };
+    #ifndef _SIZE_T
+        typedef unsigned long long size_t;
+        #define _SIZE_T size_t
+    #endif
 #else
-#error These headers are intended for C projects only.
+    #error These headers are intended for C projects only.
 #endif
-typedef unsigned char      u8;
-typedef unsigned short int u16;
-typedef unsigned int       u32;
-typedef unsigned long      u64;
-typedef unsigned long long u128;
 
-typedef char      i8;
-typedef short int i16;
-typedef int       i32;
-typedef long      i64;
-typedef long long i128;
+typedef unsigned char       u8;
+typedef unsigned short int  u16;
+typedef unsigned int        u32;
+typedef unsigned long       u64;
+typedef unsigned long long  u128;
+
+typedef char        i8;
+typedef short int   i16;
+typedef int         i32;
+typedef long        i64;
+typedef long long   i128;
 
 // floating points
-typedef float f32;
-typedef double f64;
+typedef float   f32;
+typedef double  f64;
 
-typedef enum  {
+typedef enum {
+    TYPE_NULL,
     TYPE_BOOL,
     TYPE_STR,
     TYPE_STRING,
@@ -49,6 +57,5 @@ typedef enum  {
     TYPE_F64,
 } Type;
 
-extern size_t Type_sizeof (Type);
-extern size_t Type_hash   (Type, void *);
-
+extern size_t Type_sizeof   (Type);
+extern size_t Type_hash     (Type, void*);
